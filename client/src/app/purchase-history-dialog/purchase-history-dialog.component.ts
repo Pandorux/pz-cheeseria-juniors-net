947daa67e16435ce8346cb9b77d610bbcf212aa1
@Pandorux <jared.l.j.carey@gmail.com>
2022-08-04 15:32:22 +1000

    WIP: Purchase History Dialog - Added Basic Expandable Item Grid

--- client/src/app/purchase-history-dialog/purchase-history-dialog.component.ts
+++ client/src/app/purchase-history-dialog/purchase-history-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Transaction } from '../_models/transaction';
 import { TransactionsService } from '../_services/transactions.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-purchase-history-dialog',
@@ -9,17 +10,32 @@ import { TransactionsService } from '../_services/transactions.service';
   styleUrls: [
     './../purchases.css',
     './purchase-history-dialog.component.css'
+  ],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
   ]
 })
 export class PurchaseHistoryDialogComponent implements OnInit {
 
+  expandedTransaction: Transaction | null;
   transactions: Transaction[];
   purchasesColumns: string[] = [
     'purchase-no',
     'purchase-datetime',
     'purchase-total-items',
-    'purchase-total-amount'
+    'purchase-total-amount',
+    'expand'
   ];
+  purchasedItemColumns: string[] = [
+    'item-no',
+    'item-quantity',
+    'item-price',
+    'item-total'
+  ]
 
   constructor(
       // TODO: Unsure if I need both the Dialog Ref and Dialog Data
