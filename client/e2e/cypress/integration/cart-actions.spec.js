ca8304fd58c15ededb6d3eac8bc43b789c95eab6
@Pandorux <jared.l.j.carey@gmail.com>
2022-08-05 12:56:03 +1000

    Added Cypress Tests for Cart and Recent Purchases

--- client/e2e/cypress/integration/cart-actions.spec.js
+++ client/e2e/cypress/integration/cart-actions.spec.js
@@ -11,7 +11,27 @@ context('Cart Actions', () => {
 
     cy.get('[data-cy=view-cart-btn]').click();
     cy.contains('2 Selected item')
-
   })
 
+  it.only('Purchase Items', () => {
+    cy.get('[data-cy=add-to-cart-2]').click();
+    cy.get('[data-cy=add-to-cart-3]').click();
+
+    cy.get('[data-cy=view-cart-btn]').click();
+    cy.get('[data-cy=purchase-items-btn]').click();
+
+    cy.contains('Add items to your cart and then find them here.')
+  })
+
+  it.only('Check Recent Purchase History', () => {
+    cy.get('[data-cy=view-transaction-history-btn]').click();
+
+    cy.get('[data-cy=recent-purchase-no-1]').should(($div) => {
+      expect($div.text().trim()).equal('1');
+    })
+
+    cy.get('[data-cy=recent-purchase-total-items-1]').should(($div) => {
+      expect($div.text().trim()).equal('2');
+    })
+  })
 })
